90b1858c9c2e65de74a4c2bad7eaab1c4b423fe4
Ali Karadag <ali.karadag@chronext.com>
2019-08-17 16:32:53 +0200

    imporve alerting system

--- src/components/EditableTable.vue
+++ src/components/EditableTable.vue
@@ -1,59 +1,74 @@
 <template>
-  <v-data-table :headers="headers" :items="cars" sort-by="name" class="elevation-1">
-    <template v-slot:top>
-      <v-toolbar flat color="white">
-        <v-toolbar-title>Location: Alsdorf</v-toolbar-title>
-        <v-divider class="mx-4" inset vertical></v-divider>
-        <v-spacer></v-spacer>
-        <v-dialog v-model="dialog" max-width="500px">
-          <template v-slot:activator="{ on }">
-            <v-btn color="primary" dark class="mb-2" v-on="on">New Entry</v-btn>
-          </template>
-          <v-card>
-            <v-card-title>
-              <span class="headline">{{ formTitle }}</span>
-            </v-card-title>
+  <div>
+    <template>
+      <v-alert
+        :value="errorOccurred"
+        dark
+        color="error"
+        icon="mdi-home"
+        transition="scale-transition"
+      >{{ errorMessage }}</v-alert>
+    </template>
+    <v-data-table :headers="headers" :items="cars" sort-by="name" class="elevation-1">
+      <template v-slot:top>
+        <v-toolbar flat color="white">
+          <v-toolbar-title>Location: Alsdorf</v-toolbar-title>
+          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+          <v-dialog v-model="dialog" max-width="500px">
+            <template v-slot:activator="{ on }">
+              <v-btn color="primary" dark class="mb-2" v-on="on">New Entry</v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="headline">{{ formTitle }}</span>
+              </v-card-title>
 
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col cols="12" sm="7" md="4">
-                    <v-text-field v-model="editedItem.name" label="Car name"></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="2" md="2">
-                    <v-text-field v-model="editedItem.count" label="Count"></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="9" md="6">
-                    <v-text-field v-model="editedItem.time" label="Time"></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col cols="12" sm="7" md="4">
+                      <v-text-field v-model="editedItem.name" label="Car name"></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="2" md="2">
+                      <v-text-field v-model="editedItem.count" label="Count"></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="9" md="6">
+                      <v-text-field v-model="editedItem.time" label="Time"></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
 
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-              <v-btn color="blue darken-1" :loading="loading" text @click="save">Save</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-toolbar>
-    </template>
-    <template v-slot:item.action="{ item }">
-      <v-icon small class="mr-2" @click="editItem(item)">edit</v-icon>
-      <v-icon small @click="deleteItem(item)">delete</v-icon>
-    </template>
-    <template v-slot:no-data>
-      <v-btn color="primary" @click="initialize">Reset</v-btn>
-    </template>
-  </v-data-table>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+                <v-btn color="blue darken-1" :loading="loading" text @click="save">Save</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-toolbar>
+      </template>
+      <template v-slot:item.action="{ item }">
+        <v-icon small class="mr-2" @click="editItem(item)">edit</v-icon>
+        <v-icon small @click="deleteItem(item)">delete</v-icon>
+      </template>
+      <template v-slot:no-data>
+        <v-btn color="primary" @click="initialize">Reset</v-btn>
+      </template>
+    </v-data-table>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 
+const ERR_MSG_TIMEOUT = 5000;
+
 export default {
   data: () => ({
+    errorOccurred: false,
+    errorMessage: "",
     loading: false,
     dialog: false,
     headers: [
@@ -102,6 +117,13 @@ export default {
       this.editedItem = Object.assign({}, item);
       this.dialog = true;
     },
+    displayErrorMessage(message) {
+      this.errorOccurred = true;
+      this.errorMessage = message;
+      setTimeout(() => {
+        this.errorOccurred = false;
+      }, ERR_MSG_TIMEOUT);
+    },
     deleteItem(item) {
       if (confirm("Are you sure you want to delete this item?") === true) {
         axios
@@ -112,6 +134,10 @@ export default {
           })
           .catch(error => {
             console.log(error);
+
+            this.displayErrorMessage(
+              `Error while deleting car with id: ${item._id}`
+            );
           });
       }
     },
@@ -130,6 +156,8 @@ export default {
         })
         .catch(error => {
           console.log(error);
+
+          this.displayErrorMessage("Error while loading cars.");
         });
     },
     save() {
@@ -155,6 +183,10 @@ export default {
             this.loading = false;
             this.close();
             console.log(error);
+
+            this.displayErrorMessage(
+              `Error while updating car with id: ${this.editedItem._id}`
+            );
           });
       }
       //create new entry
@@ -172,6 +204,8 @@ export default {
             this.loading = false;
             this.close();
             console.log(error);
+
+            this.displayErrorMessage("Error while saving new car.");
           });
       }
     }
