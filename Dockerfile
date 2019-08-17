a05cfbb240cbcb750cc25f30239e9ef0cc28d5a5
Ali Karadag <ali.karadag@chronext.com>
2019-08-17 22:08:53 +0200

    autofix eslint and try to use nginx in dockerfile

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,10 @@ RUN yarn global add serve
 RUN yarn add vuetify
 RUN yarn install
 COPY . ./
-CMD ["yarn", "serve"]
+RUN yarn build
+
+
+FROM nginx:stable-alpine as production-stage
+COPY /app/dist /usr/share/nginx/html
+EXPOSE 8080
+CMD ["nginx", "-g", "daemon off;"]
--- src/router.ts
+++ src/router.ts
@@ -1,14 +1,14 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Dashboard from './views/Dashboard.vue';
+import Vue from "vue";
+import Router from "vue-router";
+import Dashboard from "./views/Dashboard.vue";
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'dashboard',
+      path: "/",
+      name: "dashboard",
       component: Dashboard
     }
   ]
