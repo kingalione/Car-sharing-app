a6b292f1aa4d63043caec3b3af16cc9606e47b83
Ali Karadag <ali.karadag@chronext.com>
2019-08-17 15:10:07 +0200

    Better labeling

--- src/components/EditableTable.vue
+++ src/components/EditableTable.vue
@@ -2,12 +2,12 @@
   <v-data-table :headers="headers" :items="cars" sort-by="name" class="elevation-1">
     <template v-slot:top>
       <v-toolbar flat color="white">
-        <v-toolbar-title>My Cars</v-toolbar-title>
+        <v-toolbar-title>Location: Alsdorf</v-toolbar-title>
         <v-divider class="mx-4" inset vertical></v-divider>
         <v-spacer></v-spacer>
         <v-dialog v-model="dialog" max-width="500px">
           <template v-slot:activator="{ on }">
-            <v-btn color="primary" dark class="mb-2" v-on="on">New Item</v-btn>
+            <v-btn color="primary" dark class="mb-2" v-on="on">New Entry</v-btn>
           </template>
           <v-card>
             <v-card-title>
@@ -63,7 +63,7 @@ export default {
         value: "name"
       },
       { text: "Count", value: "count" },
-      { text: "Time", value: "time" },
+      { text: "Last update", value: "time" },
       { text: "Actions", value: "action", sortable: false }
     ],
     cars: [],
